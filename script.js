a4017f96b99ed3d878114b555e0cd99a37208de1
muhammadayan1208 <ayananas743@gmail.com>
2024-02-01 19:11:25 +0500

    Updated script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const getMean = (array) =>
+  array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {};
+
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+
+  const mean = getMean(numbers);
+
+  document.querySelector("#mean").textContent = mean;
+};
