c10ee72bf02524ddc69958aedb1fafcfb47ae2af
muhammadayan1208 <ayananas743@gmail.com>
2024-02-13 17:17:30 +0500

    docs: updated script.js

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-window.onload = () => {
-  const container = document.getElementById("container");
-  const createLabel = (name) => {
-    const label = document.createElement("div");
-    label.className = "label";
-    label.textContent = name;
-    container.appendChild(label);
-  };
-};
+const messageInput = document.getElementById("message-input");
+const result = document.getElementById("result");
+const checkMessageButton = document.getElementById("check-message-btn");
+
+const helpRegex = /please help|assist me/i;
+const dollarRegex = /[0-9]+ (hundred|thousand|million|billion)? dollars/i;
+
+const denyList = [helpRegex, dollarRegex];
+
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
+
+checkMessageButton.addEventListener("click", () => {
+  if (messageInput.value === "") {
+    alert("Please enter a message.");
+    return;
+  }
+
+  result.textContent = isSpam(messageInput.value)
+    ? "Oh no! This looks like a spam message."
+    : "This message does not seem to contain any spam.";
+  messageInput.value = "";
+});
