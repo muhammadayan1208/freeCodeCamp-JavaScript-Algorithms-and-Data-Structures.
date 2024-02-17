b64f6277e1d9e27e034db7aa6cfbf7c768e5577b
muhammadayan1208 <ayananas743@gmail.com>
2024-02-17 14:28:47 +0500

    docs: modified script.js file.

--- script.js
+++ script.js
@@ -54,6 +54,34 @@ const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+
+  if (keys.rightKey.pressed && player.position.x < 400) {
+    player.velocity.x = 5;
+  } else if (keys.leftKey.pressed && player.position.x > 100) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+  }
+};
+
+const keys = {
+  rightKey: {
+    pressed: false,
+  },
+  leftKey: {
+    pressed: false,
+  },
+};
+
+const movePlayer = (key, xVelocity, isPressed) => {
+  if (!isCheckpointCollisionDetectionActive) {
+    player.velocity.x = 0;
+    player.velocity.y = 0;
+    return;
+  }
+
+  switch (key) {
+  }
 };
 
 const startGame = () => {
