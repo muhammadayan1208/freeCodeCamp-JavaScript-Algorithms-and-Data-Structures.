d6b92456f9006f5bd05549e060f0f58c45a67cb2
muhammadayan1208 <ayananas743@gmail.com>
2024-02-21 15:28:59 +0500

    refactor: add index.html, styles.css, script.js files in src, css, scripts folders.

--- Cash-Register-Project/scripts/script.js
+++ Cash-Register-Project/scripts/script.js
@@ -16,7 +16,6 @@ const cash = document.getElementById("cash");
 const purchaseBtn = document.getElementById("purchase-btn");
 const priceScreen = document.getElementById("price-screen");
 const cashDrawerDisplay = document.getElementById("cash-drawer-display");
-
 const formatResults = (status, change) => {
   displayChangeDue.innerHTML = `<p>Status: ${status}</p>`;
   change.map(
